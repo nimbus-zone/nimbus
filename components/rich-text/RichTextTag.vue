6d7efb62d96e4c797f1107f3e39cc2f4fdc7825e
Prazdevs <hi@praz.dev>
2024-11-24 18:36:15 +0100

    feat(RichText): tag dropdown

--- components/rich-text/RichTextTag.vue
+++ components/rich-text/RichTextTag.vue
@@ -8,7 +8,25 @@ defineProps<{
 </script>
 
 <template>
-  <NuxtLink :to="`/hashtag/${tag}`">
-    {{ text }}
-  </NuxtLink>
+  <CommonDropdown inline-flex>
+    <button>{{ text }}</button>
+    <template #popper>
+      <NuxtLink :to="`/hashtag/${tag}`">
+        <CommonDropdownItem
+          :text="`See ${tag} posts`"
+          icon="i-ri:search-line"
+        />
+      </NuxtLink>
+      <NuxtLink :to="`/hashtag/${tag}`">
+        <CommonDropdownItem
+          :text="`See ${tag} posts by user`"
+          icon="i-ri:user-line"
+        />
+      </NuxtLink>
+      <CommonDropdownItem
+        :text="`Mute ${tag}`"
+        icon="i-ri:eye-off-line"
+      />
+    </template>
+  </CommonDropdown>
 </template>
